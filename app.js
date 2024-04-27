47b45810292ed3bb01dbda0622cc8c8e1404d6c3
Jayathi Mishra <jayathimishra2@gmail.com>
2024-04-27 09:06:07 +0000

    changed parent route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 app.use(express.json()); 
 
-app.use('/tasks', taskRoutes);
+app.use('/', taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
